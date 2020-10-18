ccc0653fb457d9db5a6b530ca2bdeb6cd128f864
Sebastian Wulf <sebastian.wulf@simplysoftware.de>
2020-10-18 17:21:59 +0200

    Template upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Seite 1</title>
+    <link rel="stylesheet" href="css/login.css" media="screen">
+    <link rel="stylesheet" href="css/style.css" media="screen">
+    <script class="u-script" type="text/javascript" src="js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="js/squery.js" defer=""></script>
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+  </head>
+  <body data-home-page-title="Seite 1" class="u-body"><header class="u-align-left u-clearfix u-header u-header" id="sec-c748"><div class="u-clearfix u-sheet u-sheet-1"></div></header>
+    <section class="u-align-center u-clearfix u-section-1" id="carousel_8bd8">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-align-center-sm u-align-center-xs u-border-10 u-border-palette-1-base u-container-style u-expanded-width-sm u-expanded-width-xs u-group u-group-1">
+          <div class="u-container-layout u-valign-top u-container-layout-1">
+            <h3 class="u-align-center-lg u-align-center-md u-align-center-xl u-text u-text-default u-text-1">severnauten login</h3>
+            <div class="u-form u-form-1">
+              <form action="https://nicepage.com/editor/Forms/Process" method="POST" class="u-clearfix u-form-spacing-20 u-form-vertical u-inner-form" style="padding: 10px" source="email" name="form">
+                <div class="u-form-group u-form-name u-form-group-1">
+                  <label for="name-a97b" class="u-form-control-hidden u-label">Name</label>
+                  <input type="text" placeholder="E-Mail" id="name-a97b" name="email-1" class="u-border-1 u-border-grey-30 u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle u-white" required="">
+                </div>
+                <div class="u-form-email u-form-group u-form-group-2">
+                  <label for="email-a97b" class="u-form-control-hidden u-label">Email</label>
+                  <input type="email" placeholder="Passwort" id="email-a97b" name="password" class="u-border-1 u-border-grey-30 u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle u-white" required="">
+                </div>
+                <div class="u-align-center u-form-group u-form-submit u-form-group-3">
+                  <a href="#" class="u-btn u-btn-submit u-button-style">Submit</a>
+                  <input type="submit" value="submit" class="u-form-control-hidden">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <footer class="u-align-left u-clearfix u-footer" id="sec-d7a9"><div class="u-clearfix u-sheet u-sheet-1"></div></footer>
+  </body>
+</html>
