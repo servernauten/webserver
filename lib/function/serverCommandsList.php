78d76a4d3d768f81dae3a5f6acb840f896f180df
Sebastian Wulf <sebastian.wulf@simplysoftware.de>
2020-10-25 15:32:35 +0100

    no message

---
+++ lib/function/serverCommandsList.php
@@ -0,0 +1,4 @@
+<?php
+$minecraftStart   =   'sudo screen -AmdS SCREENNAME java -Xmx1024M -Xms1024M -jar /home/deinNutzername/minecraft/minecraft_server.jar';
+$minecraftStop    =   'sudo screen -r SCREENNAME -X quit';
+?>
