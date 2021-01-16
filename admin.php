014febfd2845515ab1c973624e7c1cfa5a575ad7
servernauten_de <sebastian.wulf@simplysoftware.de>
2021-01-16 08:10:38 +0100

    Update admin.php
    admin

--- admin.php
+++ admin.php
@@ -15,8 +15,67 @@ session_start();
 // Ausgabe wenn E-Mail oder Passwort falsch
 echo $errorLogin;
 ?>
-<form method="POST" action="admin.php">
-  <input type="text" name="email" />
-  <input type="password" name="password" />
-  <input type="submit" name="login" value="Login" />
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" href="font/iconsmind-s/css/iconsminds.css" />
+    <link rel="stylesheet" href="font/simple-line-icons/css/simple-line-icons.css" />
+
+    <link rel="stylesheet" href="css/vendor/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/vendor/bootstrap.rtl.only.min.css" />
+    <link rel="stylesheet" href="css/vendor/bootstrap-float-label.min.css" />
+    <link rel="stylesheet" href="css/dore.light.bluenavy.css" />
+</head>
+
+<body class="background show-spinner no-footer">
+    <div class="fixed-background"></div>
+    <main>
+        <div class="container">
+            <div class="row h-100">
+                <div class="col-12 col-md-10 mx-auto my-auto">
+                    <div class="card auth-card">
+                        <div class="position-relative image-side ">
+
+                            <p class=" text-white h2">MAGIC IS IN THE DETAILS</p>
+
+                            <p class="white mb-0">
+                                Please use your credentials to login.
+                                <br>If you are not a member, please
+                                <a href="#" class="white">register</a>.
+                            </p>
+                        </div>
+                        <div class="form-side">
+                            <h6 class="mb-4">Login</h6>
+                            <form method="POST" action="admin.php">
+                                <label class="form-group has-float-label mb-4">
+                                    <input class="form-control" type="text" name="email" />
+                                    <span>E-mail</span>
+                                </label>
+
+                                <label class="form-group has-float-label mb-4">
+                                    <input class="form-control" type="password" name="password" placeholder="" />
+                                    <span>Password</span>
+                                </label>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#">Forget password?</a>
+                                    <input class="btn btn-primary btn-lg btn-shadow" type="submit" name="login" value="Login"></input>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="js/vendor/jquery-3.3.1.min.js"></script>
+    <script src="js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="js/dore.script.js"></script>
+    <script src="js/scripts.single.theme.js"></script>
+</body>
+
+</html>
