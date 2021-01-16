da9bdb1320ac99936453e3d5137345804b612df9
servernauten_de <sebastian.wulf@simplysoftware.de>
2021-01-16 08:28:59 +0100

    Dateien gelöscht

--- admin.php
+++ admin.php
@@ -40,14 +40,6 @@ echo $errorLogin;
                 <div class="col-12 col-md-10 mx-auto my-auto">
                     <div class="card auth-card">
                         <div class="position-relative image-side ">
-
-                            <p class=" text-white h2">MAGIC IS IN THE DETAILS</p>
-
-                            <p class="white mb-0">
-                                Please use your credentials to login.
-                                <br>If you are not a member, please
-                                <a href="#" class="white">register</a>.
-                            </p>
                         </div>
                         <div class="form-side">
                             <h6 class="mb-4">Login</h6>
